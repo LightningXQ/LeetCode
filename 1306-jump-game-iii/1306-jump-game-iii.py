660cb22141715b94bb97fdac48ffca6ca73f5ebb
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-05-18 15:01:39 +0900

    Time: 11 ms (65.56%), Space: 26.6 MB (47.42%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canReach(self, arr: list[int], start: int) -> bool:
+        n = len(arr)
+
+        visited = set()
+        curr = [start]
+
+        while curr:
+            nex = list()
+
+            for node in curr:
+                if arr[node] == 0: return True
+
+                for child in [node - arr[node], node + arr[node]]:
+                    if 0 <= child <= n - 1 and child not in visited:
+                        visited.add(child)
+                        nex.append(child)
+
+            curr = nex
+
+        return False
