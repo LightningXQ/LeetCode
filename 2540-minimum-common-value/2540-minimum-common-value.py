8f229a600979eb7b6ee04334733bcdc96f7ffc0e
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-05-19 14:53:09 +0900

    Time: 8 ms (64.22%), Space: 38.8 MB (25.39%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
+        iter1 = iter(nums1)
+        iter2 = iter(nums2)
+
+        visited_1 = set()
+        visited_2 = set()
+
+        a = next(iter1)
+        b = next(iter2)
+
+        while True:
+            if a == b: return a
+            else:
+                visited_1.add(a)
+                visited_2.add(b)
+
+                if a > b:
+                    try:
+                        while b in visited_2: b = next(iter2)
+                    except StopIteration:
+                        return -1
+                else:
+                    try:
+                        while a in visited_1: a = next(iter1)
+                    except StopIteration:
+                        return -1
