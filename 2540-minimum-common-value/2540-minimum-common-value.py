9d8fd38a077726d4bbaca2e01669ee8593cb737f
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-05-19 15:11:32 +0900

    Time: 15 ms (23.33%), Space: 38.8 MB (25.39%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,7 +1,28 @@
 class Solution:
     def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
-        set1 = set(nums1)
-        set2 = set(nums2)
-        intersection = set1 & set2
+        bias = nums1[-1]
 
-        return min(intersection) if len(intersection) else -1
+        visited = set()
+
+        iter1 = iter(nums1)
+        iter2 = iter(nums2)
+
+        a = next(iter1)
+        b = next(iter2)
+
+        while True:
+            if a == b: return a
+            else:
+                visited.add(a)
+                visited.add(b + bias)
+
+                if a > b:
+                    try:
+                        while b + bias in visited: b = next(iter2)
+                    except StopIteration:
+                        return -1
+                else:
+                    try:
+                        while a in visited: a = next(iter1)
+                    except StopIteration:
+                        return -1
