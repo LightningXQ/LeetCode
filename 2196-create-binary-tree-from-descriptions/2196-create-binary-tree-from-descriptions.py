b37df7a297ce49ea7bb72479ba964210ec8e57ed
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-12 16:09:04 +0900

    Time: 142 ms (49.29%), Space: 27.7 MB (37.4%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -9,43 +9,16 @@
 class Solution:
     def createBinaryTree(self, descriptions: list[list[int]]) -> TreeNode | None:
         nodes = dict()
+        children = set()
 
-        for d in descriptions:
-            val, child, isLeft = d
+        for parent, child, isLeft in descriptions:
+            if parent not in nodes: nodes[parent] = TreeNode(parent)
+            if child not in nodes: nodes[child] = TreeNode(child)
 
-            if val not in nodes:
-                if isLeft:  nodes[val] = [None, child, None]
-                else:       nodes[val] = [None, None, child]
-            else:
-                if isLeft:  nodes[val][1] = child
-                else:       nodes[val][2] = child
+            if isLeft: nodes[parent].left = nodes[child]
+            else: nodes[parent].right = nodes[child]
+
+            children.add(child)
 
         for node_val in nodes:
-            n = nodes[node_val]
-            parent, left, right = n
-            if left is not None and left in nodes:
-                nodes[left][0] = node_val
-            if right is not None and right in nodes:
-                nodes[right][0] = node_val
-
-        root = None
-        for node_val in nodes:
-            node = nodes[node_val]
-            if node[0] is None:
-                root = node_val
-                break
-
-        def createBinaryTreeFromDict(node_val):
-            if node_val in nodes:
-                left = nodes[node_val][1]
-                right = nodes[node_val][2]
-            else: left, right = None, None
-            left_node, right_node = None, None
-            if left is not None: left_node = createBinaryTreeFromDict(left)
-            if right is not None: right_node = createBinaryTreeFromDict(right)
-            node = TreeNode(node_val, left_node, right_node)
-            return node
-
-        tree_root = createBinaryTreeFromDict(root)
-
-        return tree_root
+            if node_val not in children: return nodes[node_val]
