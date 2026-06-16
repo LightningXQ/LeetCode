b12b6f4f2f88d60118a99f0975fc05ad642117f9
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-16 15:19:45 +0900

    Time: 85 ms (26.86%), Space: 34 MB (55.51%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -1,8 +1,15 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: list[int]) -> bool:
-        asteroids.sort()
+        ast_curr = asteroids.copy()
+        ast_prev = ast_curr.copy()
         curr_mass = mass
-        for a in asteroids:
-            if curr_mass < a: return False
-            curr_mass += a
-        return True
+
+        while True:
+            for i, a in enumerate(ast_curr):
+                if curr_mass >= a:
+                    curr_mass += a
+                    ast_curr[i] = 0
+            if not any(ast_curr): return True
+            elif ast_prev == ast_curr: return False
+            else: ast_prev = ast_curr.copy()
+
