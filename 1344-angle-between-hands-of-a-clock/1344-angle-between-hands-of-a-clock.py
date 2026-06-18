d4e34baa3cf6d1f48ff21e467ac6ab622b4c84b6
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-18 14:32:20 +0900

    Time: 0 ms (100%), Space: 19.5 MB (12.42%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
@@ -1,9 +1,6 @@
 class Solution:
     def angleClock(self, hour: int, minutes: int) -> float:
-        hour_pos = (hour % 12) * 5 + (minutes / 60) * 5
-        minutes_pos = minutes
-
-        angle = abs(hour_pos - minutes_pos)
-        if angle > 30: angle = 60 - angle
+        angle = abs((hour % 12) * 5 + (minutes / 60) * 5 - minutes)
+        angle += (60 - 2 * angle) * (angle > 30)
         angle *= 6
         return angle
