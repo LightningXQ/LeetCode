83ea1ac0d821702d6b8dda1701bb7d9bb27a5cc6
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-19 15:26:38 +0900

    Time: 7 ms (85.11%), Space: 19.3 MB (32.84%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -1,16 +1,11 @@
 class Solution:
     def mapWordWeights(self, words: list[str], weights: list[int]) -> str:
-        dct = dict()
-        val = 0
-        for c in range(ord("z"), ord("a") - 1, -1):
-            dct[val] = chr(c)
-            val += 1
+        num_to_alphabet = [chr(c) for c in range(ord("z"), ord("a") - 1, -1)]
 
         result = list()
         for w in words:
             tmp = 0
             for c in w: tmp += weights[ord(c) - 97]
-            result.append(dct[tmp % 26])
+            result.append(num_to_alphabet[tmp % 26])
 
         return "".join(result)
-    
