6a0300d860f61551820bb87562aa31570fe717b6
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-24 14:50:06 +0900

    Time: 229 ms (83.68%), Space: 19.3 MB (69.2%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,35 +4,36 @@ class Solution:
         max_len = 0
         result = ""
 
-        # odd-length palindrome
         for i in range(n):
-            l = 1
+            # odd-length palindrome
+            l_odd = 1
             pivot_l = i - 1
             pivot_r = i + 1
 
             while pivot_l >= 0 and pivot_r <= n - 1 and s[pivot_l] == s[pivot_r]:
-                l += 2
+                l_odd += 2
                 pivot_l -= 1
                 pivot_r += 1
 
-            if l > max_len:
+            if l_odd > max_len:
                 result = s[pivot_l + 1:pivot_r]
-                max_len = l
+                max_len = l_odd
 
-        # even-length palindrome
-        for i in range(n - 1):
-            l = 0
+            if i == n - 1: break
+
+            # even-length palindrome
+            l_even = 0
             pivot_l = i
             pivot_r = i + 1
 
             while pivot_l >= 0 and pivot_r <= n - 1 and s[pivot_l] == s[pivot_r]:
-                l += 2
+                l_even += 2
                 pivot_l -= 1
                 pivot_r += 1
 
-            if l > max_len:
+            if l_even > max_len:
                 result = s[pivot_l + 1:pivot_r]
-                max_len = l
+                max_len = l_even
 
         return result
 
