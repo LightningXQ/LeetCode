9255bffe915de649a7ebd6ea82e317991f6b51a8
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-26 17:46:25 +0900

    Time: 64 ms (17.25%), Space: 32.8 MB (5.32%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        n = len(height)
         max_height = max(height)
         h_map = [[] for _ in range(max_height + 1)]
 
@@ -11,6 +12,7 @@ class Solution:
 
         for h, idxs in enumerate(h_map[::-1]):
             h = max_height - h
+            if max_vol >= h * (n - 1): break 
             if not idxs: continue
             min_i, max_i = min(min(idxs), min_i), max(max(idxs), max_i)
             max_vol = max(max_vol, h * (max_i - min_i))
