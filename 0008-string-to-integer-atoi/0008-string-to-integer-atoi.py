1542925df4f6ad04b596d5c892f2f3d7daad07cb
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-26 16:40:30 +0900

    Time: 0 ms (100%), Space: 19.2 MB (95.89%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,6 +1,5 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        has_whitespace = False
         has_signed = False
         has_zero_leading = False
         has_number = False
@@ -13,20 +12,25 @@ class Solution:
 
             if c == " ":
                 if has_signed or has_zero_leading or has_number: break
-                else: has_whitespace = True; continue
 
             if c == "+" or c == "-":
                 if has_signed or has_zero_leading or has_number: break
-                else: 
+                else:
                     if c == "-": lst.append(c)
-                    has_signed = True; continue
-            
+                    has_signed = True
+                    continue
+
             if c == "0":
                 if has_number: lst.append(c)
-                else: has_zero_leading = True; continue
-            
-            if "1" <= c <= "9": lst.append(c); has_number = True; is_number_in = True
-        
+                else:
+                    has_zero_leading = True
+                    continue
+
+            if "1" <= c <= "9":
+                lst.append(c)
+                has_number = True
+                is_number_in = True
+
         if not is_number_in: return 0
         ans = int("".join(lst))
         if ans < -(2 ** 31): ans = -(2 ** 31)
