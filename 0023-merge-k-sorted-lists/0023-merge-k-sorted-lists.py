d5ac14c61a36f6f0de08924bce02f138770fcfa4
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-30 16:02:09 +0900

    Time: 8 ms (70.07%), Space: 23 MB (29.17%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -9,24 +9,14 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists or not any(lists): return None
 
-        n = len(lists)
-        done = [False] * n
         result = list()
 
-        while True:
-            min_val = 10 ** 4 + 1
-            min_val_idx = 10 ** 4
+        for lst in lists:
+            while lst:
+                result.append(lst.val)
+                lst = lst.next
 
-            for i in range(n):
-                if not lists[i]: continue
-                if min_val > lists[i].val:
-                    min_val = lists[i].val
-                    min_val_idx = i
-
-            if min_val == 10 ** 4 + 1: break
-
-            result.append(min_val)
-            lists[min_val_idx] = lists[min_val_idx].next
+        result.sort()
 
         head = ListNode()
         head.val = result[0]
