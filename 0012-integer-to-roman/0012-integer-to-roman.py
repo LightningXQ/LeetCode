191a78f691a59bd4e957c9659ed904dbf3701572
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-30 14:11:19 +0900

    Time: 4 ms (61.76%), Space: 19.4 MB (25.6%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,57 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        result = list()
+
+        while num >= 1000:
+            num -= 1000
+            result.append("M")
+        
+        if num >= 900:
+            num -= 900
+            result.append("CM")
+        
+        while num >= 500:
+            num -= 500
+            result.append("D")
+        
+        if num >= 400:
+            num -= 400
+            result.append("CD")
+
+        while num >= 100:
+            num -= 100
+            result.append("C")
+        
+        if num >= 90:
+            num -= 90
+            result.append("XC")
+
+        while num >= 50:
+            num -= 50
+            result.append("L")
+        
+        if num >= 40:
+            num -= 40
+            result.append("XL")
+        
+        while num >= 10:
+            num -= 10
+            result.append("X")
+        
+        if num >= 9:
+            num -= 9
+            result.append("IX")
+        
+        while num >= 5:
+            num -= 5
+            result.append("V")
+        
+        if num >= 4:
+            num -= 4
+            result.append("IV")
+        
+        while num >= 1:
+            num -= 1
+            result.append("I")
+        
+        return "".join(result)
