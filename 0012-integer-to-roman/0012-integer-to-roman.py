3e78e4dc1f4ad78596be556aaad012397c625486
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-06-30 14:13:00 +0900

    Time: 1 ms (84.99%), Space: 19.3 MB (25.6%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -9,49 +9,49 @@ class Solution:
         if num >= 900:
             num -= 900
             result.append("CM")
-        
-        while num >= 500:
-            num -= 500
-            result.append("D")
+        else:
+            while num >= 500:
+                num -= 500
+                result.append("D")
         
         if num >= 400:
             num -= 400
             result.append("CD")
-
-        while num >= 100:
-            num -= 100
-            result.append("C")
+        else:
+            while num >= 100:
+                num -= 100
+                result.append("C")
         
         if num >= 90:
             num -= 90
             result.append("XC")
-
-        while num >= 50:
-            num -= 50
-            result.append("L")
+        else:
+            while num >= 50:
+                num -= 50
+                result.append("L")
         
         if num >= 40:
             num -= 40
             result.append("XL")
-        
-        while num >= 10:
-            num -= 10
-            result.append("X")
+        else:
+            while num >= 10:
+                num -= 10
+                result.append("X")
         
         if num >= 9:
             num -= 9
             result.append("IX")
-        
-        while num >= 5:
-            num -= 5
-            result.append("V")
+        else:
+            while num >= 5:
+                num -= 5
+                result.append("V")
         
         if num >= 4:
             num -= 4
             result.append("IV")
-        
-        while num >= 1:
-            num -= 1
-            result.append("I")
-        
+        else:
+            while num >= 1:
+                num -= 1
+                result.append("I")
+            
         return "".join(result)
