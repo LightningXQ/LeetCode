d6d28f38d87e690f6341cbe5464071c15aa437d7
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-07-06 16:59:39 +0900

    Time: 4 ms (6.03%), Space: 19.2 MB (65.47%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,26 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        sign = -1 if ((dividend > 0) ^ (divisor > 0)) else 1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        result = 0
+        while dividend >= divisor:
+            d = divisor
+
+            amplifier = 1
+            while dividend >= d * 2: 
+                d *= 2 
+                amplifier *= 2
+
+            dividend -= d
+            result += amplifier
+        
+        result *= sign
+
+        minimum = -1 * (2 ** 31)
+        maximum = 2 ** 31 - 1
+        result = minimum if result < minimum else result
+        result = maximum if result > maximum else result
+
+        return result
