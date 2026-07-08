4a58176213ba59b2174080a93a49992694e0b3e5
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-07-08 14:23:14 +0900

    Time: 1 ms (83.24%), Space: 19.4 MB (9.42%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,23 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        secs = [set() for _ in range(9)]
+        # rows = [[False] * 9 for _ in range(9)]
+        # cols = [[False] * 9 for _ in range(9)]
+        # secs = [[False] * 9 for _ in range(9)]
+        bundle = [[[False] * 9 for _ in range(9)] for _ in range(3)]
 
         for i in range(9):
             for j in range(9):
-                num = board[i][j]
-                if num == ".": continue
+                if (elm := board[i][j]) == ".": continue
+                num = int(elm) - 1
                 
                 r = j
                 c = i
                 s = (i // 3) * 3 + (j // 3)
 
-                if num in rows[r] or num in cols[c] or num in secs[s]: 
-                    return False
-
-                rows[r].add(num)
-                cols[c].add(num)
-                secs[s].add(num)
+                for idx, k in enumerate([r, c, s]):
+                    if bundle[idx][k][num]: return False
+                    bundle[idx][k][num] = True
 
         return True
