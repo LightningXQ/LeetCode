7ec2c8ef08a400fd31d46dad4b79bf9126f7f1ae
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-07-10 09:32:19 +0900

    Time: 87 ms (64.52%), Space: 50.2 MB (45.16%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -0,0 +1,12 @@
+from bisect import bisect_left, bisect_right
+
+
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        g = [0] * n
+
+        for i in range(1, n):
+            g[i] = g[i - 1] + (nums[i] - nums[i - 1] > maxDiff)
+        
+        return [g[i] == g[j] for i, j in queries]
+        
