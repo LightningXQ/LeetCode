0594d95715875881cecbc42e4ca96192fbb584d5
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-07-13 19:50:20 +0900

    Time: 52 ms (97.23%), Space: 21.5 MB (60.89%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -8,7 +8,6 @@ class Solution:
             l = word.find(small)
             m = word.find(big)
             r = word[m:].find(small)
-            print(l, m, r)
 
             if l != -1 and m != -1 and r == -1: result += 1
         
