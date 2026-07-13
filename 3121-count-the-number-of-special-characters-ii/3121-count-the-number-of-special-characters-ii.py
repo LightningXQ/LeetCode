50466d93e3f0f5daa7089d269eb55383558e0b04
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-07-13 17:07:09 +0900

    Time: 1847 ms (5%), Space: 21.5 MB (60.89%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -2,15 +2,15 @@ class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         states = [0] * 26
 
-        for c in word:
-            c = ord(c)
-            if 97 <= c <= 122:
-                c -= 97
-                if states[c] == 0: states[c] = 1
-                if states[c] == 2: states[c] = 3
-            if 65 <= c <= 90:
-                c -= 65
-                if states[c] == 0: states[c] = 3
-                if states[c] == 1: states[c] = 2
+        for elm in range(26):
+            small, large = chr(elm + 97), chr(elm + 65)
 
+            for c in word:
+                if c == small:
+                    if states[elm] == 0: states[elm] = 1
+                    if states[elm] == 2: states[elm] = 3
+                if c == large:
+                    if states[elm] == 0: states[elm] = 3
+                    if states[elm] == 1: states[elm] = 2
+        
         return states.count(2)
