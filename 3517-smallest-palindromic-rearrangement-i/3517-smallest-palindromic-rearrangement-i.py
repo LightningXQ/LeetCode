cf6dff9ddc8ba3673c6a7ff64180f68784494a0f
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-07-28 12:53:59 +0900

    Time: 113 ms (98.62%), Space: 20.8 MB (79.72%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -3,19 +3,14 @@ class Solution:
         n = len(s)
         a = ord('a')
         d = [0] * 26
-        for c in s:
+
+        for c in s[0:n // 2]:
             d[ord(c) - a] += 1
         
         string = ""
-        odd = -1
-        for e in range(26):
-            i = d[e]
-            if i % 2: odd = e
-            string += (chr(e + a) * (i // 2))
+        for i in range(26):
+            string += (chr(i + a) * d[i])
         
-        if odd == -1: return string + string[::-1]
-        else: return string + chr(odd + a) + string[::-1]
-
-        
-
+        if n % 2: return string + s[n // 2] + string[::-1]
+        else: return string + string[::-1]
         
