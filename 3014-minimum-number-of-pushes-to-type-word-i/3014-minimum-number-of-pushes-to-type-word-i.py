a0c9fa5b5221511181147f55c2e1e0b0e21e41aa
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-07-30 09:53:18 +0900

    Time: 0 ms (100%), Space: 19.3 MB (48.74%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -2,16 +2,20 @@ class Solution:
     def minimumPushes(self, word: str) -> int:
         n = len(word)
 
-        # if n < 9:       return n
-        # elif n < 17:    return 2 * n - 8    # n + (n - 8)
-        # elif n < 25:    return 3 * n - 24   # n + (n - 8) + (n - 16)
-        # else:           return 4 * n - 48   # n + (n - 8) + (n - 16) + (n - 24)
+        # Approach 1
 
-        d = [
-            None, 1, 2, 3, 4, 5, 6, 7, 8, 
-            10, 12, 14, 16, 18, 20, 22, 24, 
-            27, 30, 33, 36, 39, 42, 45, 48, 
-            52, 56
-        ]
+        if n < 9:       return n
+        elif n < 17:    return 2 * n - 8    # n + (n - 8)
+        elif n < 25:    return 3 * n - 24   # n + (n - 8) + (n - 16)
+        else:           return 4 * n - 48   # n + (n - 8) + (n - 16) + (n - 24)
 
-        return d[n]
+        # Approach 2
+
+        # d = [
+        #     None, 1, 2, 3, 4, 5, 6, 7, 8, 
+        #     10, 12, 14, 16, 18, 20, 22, 24, 
+        #     27, 30, 33, 36, 39, 42, 45, 48, 
+        #     52, 56
+        # ]
+
+        # return d[n]
