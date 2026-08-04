a39944014884532c768bc89bb42c7efbe2ef1da7
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-08-04 10:24:03 +0900

    Time: 395 ms (15.41%), Space: 19.3 MB (81.39%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,10 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n % 2 == 0: return True
+
+        def solve(l, r):
+            if l == r: return nums[l]
+            return max(nums[l] - solve(l + 1, r), nums[r] - solve(l, r - 1))
+        
+        return True if solve(0, n - 1) >= 0 else False
