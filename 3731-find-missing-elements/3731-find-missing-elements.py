c4b4e505da9cdafd281d2e0b854329af6f4f4614
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-08-04 09:39:44 +0900

    Time: 0 ms (100%), Space: 19.3 MB (18.85%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        l, h = 101, 0
+        elms = [False] * 101
+
+        for num in nums:
+            if num > h: h = num
+            if num < l: l = num
+            elms[num] = True
+        
+        result = list()
+        for i in range(l, h + 1):
+            if not elms[i]: result.append(i)
+        
+        return result
+        
