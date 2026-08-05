688cd2e67bc36a4058f8939435f85bde224c0468
JHJeong <127932438+LightningXQ@users.noreply.github.com>
2026-08-05 13:52:56 +0900

    Time: 0 ms (100%), Space: 19 MB (98.76%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        count = 0
+        i = 1
+        while i <= n:
+            div = i * 10
+            count += int(n / div) * i + min(max(n % div - i + 1, 0), i)
+            i *= 10
+        
+        return count
